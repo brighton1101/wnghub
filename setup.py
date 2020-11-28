445f43ffcd0c50ec48c0fc81830d84af37c12931
Brighton Balfrey <balfrey@usc.edu>
2020-11-27 21:35:25 -0800

    added console script for setup.py so wnghub can be invoked directly

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ setup(
     author="Brighton Balfrey",
     author_email="balfrey@usc.edu",
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'wnghub = wnghub.__main__:main',
+        ],
+    },
     install_requires=[
         "marshmallow==3.9.1",
         "click==7.1.2",
--- wnghub/__main__.py
+++ wnghub/__main__.py
@@ -1,6 +1,18 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from wnghub.cli.base import cli
 
-if __name__ == "__main__":
+
+def main():
+    """
+    This method is the entrypoint for the `wnghub` console script.
+    """
     cli()
+
+
+if __name__ == "__main__":
+    """
+    This should be here in case users do `python3 -m wnghub`
+    Not sure who would do that, but...
+    """
+    main()
