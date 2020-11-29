7dfb205be8f19466ad94ba8fcb7cc075c73ab70f
Brighton Balfrey <balfrey@usc.edu>
2020-11-28 17:08:35 -0800

    Added ghub action to mimic pre-commit (#18)
    * added ghub action to mimic pre-commit
    
    * updated bug in github action
    
    * reran black. fixed ghub action. updated flake8
    
    * removed 3.8

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -6,7 +6,7 @@ from unittest.mock import MagicMock
 def test_set_auth_success():
     a = Config()
     a.write = MagicMock(return_value=None)
-    a.auth_token = 'hello world'
-    a.set_auth(auth_token = 'yay')
+    a.auth_token = "hello world"
+    a.set_auth(auth_token="yay")
     a.write.assert_called_once()
-    assert a.auth_token == 'yay'
+    assert a.auth_token == "yay"
