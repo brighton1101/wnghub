81ecb065cb9078bb37f9ab564298dc1cc480f858
Brighton Balfrey <balfrey@usc.edu>
2020-12-01 21:52:03 -0800

    added error handling for missing and invalid auth. Closes #26

--- wnghub/client/github.py
+++ wnghub/client/github.py
@@ -12,6 +12,10 @@ class BaseGithubClient(ABC):
     auth_token: str = ""
 
     def __init__(self, auth_token: str):
+        if auth_token is None or auth_token == "":
+            raise BadCredentialsError(
+                "Missing auth token. Please set valid " "auth token and try again."
+            )
         self.auth_token = auth_token
 
     @abstractmethod
@@ -29,7 +33,11 @@ class BaseGithubClient(ABC):
 
 class GithubApiClient(BaseGithubClient):
 
-    NOTIFICATIONS_URL = "https://api.github.com/notifications"
+    _notifications_url = "https://api.github.com/notifications"
+
+    _unauthorized_code = 401
+
+    _auth_token_info_url = "https://docs.github.com/en/free-pro-team@latest/github/authenticating-to-github/creating-a-personal-access-token"  # noqa
 
     @lru_cache(maxsize=None)
     def get_notifications(
@@ -63,7 +71,8 @@ class GithubApiClient(BaseGithubClient):
             per_page=per_page,
             page=page,
         )
-        return Notification.load_from_json_str(raw_res)
+        res = Notification.load_from_json_str(raw_res)
+        return res
 
     @lru_cache(maxsize=None)
     def _notifications(
@@ -107,5 +116,22 @@ class GithubApiClient(BaseGithubClient):
             raise Exception(
                 "Github API support maximum 100 notifications per page for api calls"
             )
-        res = request("GET", self.NOTIFICATIONS_URL, headers=headers, params=params)
+        res = request("GET", self._notifications_url, headers=headers, params=params)
+        status_code = res.status_code
+        if status_code == self._unauthorized_code:
+            raise BadCredentialsError(
+                "Invalid auth token supplied. Please "
+                "ensure valid Github personal auth token "
+                "is present. See {} for more info".format(self._auth_token_info_url)
+            )
+        elif status_code != 200:
+            raise GithubHttpException("Unknown error occurred with Github API.")
         return res.text
+
+
+class BadCredentialsError(Exception):
+    pass
+
+
+class GithubHttpException(Exception):
+    pass
